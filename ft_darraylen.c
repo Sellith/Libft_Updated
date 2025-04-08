8e44649d8ed11234ca7ceddc3e68ddaef98a5775
Leane / Sellith <leanevanbree@gmail.com>
2025-04-08 18:43:31 +0200

    Modified: ft_darraylen

--- ft_darraylen.c
+++ ft_darraylen.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 13:35:03 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/08 00:05:29 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/04/08 18:43:03 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_darraylen(char **array)
 	int	i;
 
 	i = 0;
+	if (!array)
+		return (i);
 	while (array[i])
 		i++;
 	return (i);
