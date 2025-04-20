8944abffa1b2e413de0fe3edca705dab22b620ba
Leane / Sellith <leanevanbree@gmail.com>
2025-04-20 03:03:32 +0200

    ud:ft_str_replace.c

--- ft_str_replace.c
+++ ft_str_replace.c
@@ -6,13 +6,13 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 02:52:25 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/20 03:01:04 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/04/20 03:03:06 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_str_replace(char *sent)
+char	*ft_str_replace(char *sent, int replaced)
 {
 	char	*buffer;
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 00:10:32 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/20 02:56:20 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/04/20 03:03:21 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,6 +86,6 @@ char	**ft_addtoda(char **darray, char *str);
 int		ft_strncmpcat(const char *s1, const char *s2, char *joined, size_t n);
 char	*ft_strndup(char *sent, int size);
 char	*ft_rmchars(char *str, char c);
-char	*ft_str_replace(char *sent);
+char	*ft_str_replace(char *sent, int replaced);
 
 #endif
