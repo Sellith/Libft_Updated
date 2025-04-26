fd68cd92e6aba611b73f80486658e696ade1b07c
Leane / Sellith <leanevanbree@gmail.com>
2025-04-27 01:00:54 +0200

    Fixed ft_strncmpcat.c bug

--- ft_strncmpcat.c
+++ ft_strncmpcat.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 19:13:24 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/27 00:54:57 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/04/27 01:00:21 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_strncmpcat(const char *s1, const char *s2, char *joined, size_t n)
 {
 	size_t	i;
 	char	*buffer;
+	char	c;
 
 	i = 0;
 	if (!s1 || !s2 || !joined)
@@ -25,8 +26,9 @@ int	ft_strncmpcat(const char *s1, const char *s2, char *joined, size_t n)
 	{
 		if (buffer[i] != s2[i])
 		{
+			c = buffer[i];
 			free(buffer);
-			return ((const unsigned char)buffer[i] - (const unsigned char)s2[i]);
+			return ((const unsigned char)c - (const unsigned char)s2[i]);
 		}
 		i++;
 	}
