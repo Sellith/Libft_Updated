7e148e9dc116c660481e395e7644a61fc58d6fca
Leane / Sellith <leanevanbree@gmail.com>
2025-04-28 01:58:57 +0200

    Ud: ft_str_replace.c

--- ft_str_replace.c
+++ ft_str_replace.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 02:52:25 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/24 20:16:47 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/04/28 01:58:35 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_str_replace(char *sent, int replaced)
 	char	*buffer;
 	char	*tmp;
 
+	if (!sent)
+		return (ft_strdup(""));
 	tmp = sent + replaced;
 	buffer = ft_strdup(tmp);
 	if (sent)
