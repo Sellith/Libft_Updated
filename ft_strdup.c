c0c256e84056978e16e8bcac654c88bee350f4bc
Leane / Sellith <leanevanbree@gmail.com>
2025-05-01 19:55:51 +0200

    undo strdup changes

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 03:55:04 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/05/01 03:11:08 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/01 19:55:11 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ char	*ft_strdup(const char *s)
 	if (!s)
 		return (NULL);
 	len = ft_strlen(s);
-	if (len == 0)
-		return (NULL);
 	dest = ft_calloc(len + 1, sizeof(char));
 	if (!dest)
 		return (NULL);
