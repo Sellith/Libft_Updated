2c0ec936fccec5adb7ca476eff26d2bb0de41954
Leane / Sellith <leanevanbree@gmail.com>
2025-05-01 03:11:34 +0200

    Ud: strdup return NULL if s len is 0

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 03:55:04 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/25 01:52:37 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/01 03:11:08 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 char	*ft_strdup(const char *s)
 {
 	char	*dest;
+	int		len;
 
 	if (!s)
 		return (NULL);
-	dest = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	len = ft_strlen(s);
+	if (len == 0)
+		return (NULL);
+	dest = ft_calloc(len + 1, sizeof(char));
 	if (!dest)
 		return (NULL);
 	ft_strcpy(dest, s);
