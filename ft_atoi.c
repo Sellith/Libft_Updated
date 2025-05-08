f209ad6025c11f666b80dc32a795c686b5fe2648
Leane / Sellith <leanevanbree@gmail.com>
2025-05-08 21:24:14 +0200

    ft_atoi now handles NULL strings

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lvan-bre <lvan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 02:10:49 by lvan-bre          #+#    #+#             */
-/*   Updated: 2024/10/24 03:46:40 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/08 21:23:53 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	res = 0;
 	neg = 0;
+	if (!nptr)
+		return (0);
 	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
@@ -30,10 +32,7 @@ int	ft_atoi(const char *nptr)
 		i++;
 	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		res = res * 10 + (nptr[i] - '0');
-		i++;
-	}
+		res = res * 10 + (nptr[i++] - '0');
 	if (neg > 0)
 		res *= -1;
 	return (res);
