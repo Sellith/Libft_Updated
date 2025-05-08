92e11b183927942d4af688ac974bab484a9e3ea9
Leane / Sellith <leanevanbree@gmail.com>
2025-05-08 17:34:19 +0200

    ud: ft_freedarray.c : now put darray to NULL after freeing

--- ft_freedarray.c
+++ ft_freedarray.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:35:39 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/04/08 00:08:16 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/08 17:33:30 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	ft_freedarray(char **darray)
 
 	i = 0;
 	while (darray[i])
-		free(darray[i++]);
+		ft_str_reset(darray[i++]);
 	free(darray);
+	darray = NULL;
 }
