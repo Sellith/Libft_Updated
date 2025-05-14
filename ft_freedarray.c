31ed7e1fa69580b515c9af21aaa62a4468a63db3
Leane / Sellith <leanevanbree@gmail.com>
2025-05-14 03:26:22 +0200

    ud: freedarray.c: free if darray is not NULL

--- ft_freedarray.c
+++ ft_freedarray.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:35:39 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/05/08 17:35:16 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/14 03:25:02 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_freedarray(char **darray)
 	int	i;
 
 	i = 0;
+	if (!darray)
+		return ;
 	while (darray[i])
 		ft_str_reset(&darray[i++]);
 	free(darray);
