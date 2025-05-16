6d47c8627f65164f05a8a8c62233fe497214c54d
Leane / Sellith <leanevanbree@gmail.com>
2025-05-16 02:51:32 +0200

    ud: ft_freeall error update

--- ft_freeall.c
+++ ft_freeall.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 23:37:20 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/05/16 01:43:21 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/16 02:51:06 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ static void	selec(char c, va_list args)
 		ft_str_reset(va_arg(args, char **));
 	else if (c == 'd')
 		ft_darray_reset(va_arg(args, char ***));
-	else if (c == 'S')
+	else if (c == 'm')
 		ft_mem_reset(va_arg(args, void **));
 	else if (c == 'c')
 		ft_close(va_arg(args, int *));
+	else
+		ft_printf("%e", "Error: unreconized format");
 }
 
 void	ft_freeall(char *format, ...)
@@ -41,7 +43,7 @@ void	ft_freeall(char *format, ...)
 				selec(format[i], args);
 			else
 			{
-				ft_printf_putstr("Erreur : '%' seul");
+				ft_printf("%e", "Error: incomplete format specifier");
 				return ;
 			}
 		}
