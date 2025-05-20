a40dc7ceadb20d087bd3b36ee3284d4105fbe46c
Leane / Sellith <leanevanbree@gmail.com>
2025-05-20 18:49:14 +0200

    ud: ft_dup_close.c bug fixing

--- ft_dup_close.c
+++ ft_dup_close.c
@@ -6,7 +6,7 @@
 /*   By: lvan-bre <lvan-bre@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 18:24:40 by lvan-bre          #+#    #+#             */
-/*   Updated: 2025/05/20 18:30:54 by lvan-bre         ###   ########.fr       */
+/*   Updated: 2025/05/20 18:43:51 by lvan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	ft_dup_close(int fd_src, int fd_dest)
 {
 	dup2(fd_src, fd_dest);
-	ft_close(fd_src);
+	ft_close(&fd_src);
 }
